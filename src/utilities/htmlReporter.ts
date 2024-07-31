008ed284715de2a7e99e8046c2feaa8b21ffd377
Anjali-Jain08 <60877575+Anjali-Jain08@users.noreply.github.com>
2024-07-31 16:57:09 +0530

    Update htmlReporter.ts

--- src/utilities/htmlReporter.ts
+++ src/utilities/htmlReporter.ts
@@ -1,14 +1,14 @@
-// /* eslint-disable-next-line @typescript-eslint/no-var-requires */
-// const reporter = require("cucumber-html-reporter");
+/* eslint-disable-next-line @typescript-eslint/no-var-requires */
+const reporter = require("cucumber-html-reporter");
 
-// const options = {
-//     theme: "bootstrap",
-//     jsonDir: "src/reports/",
-//     output: "src/reports/cucumber_report.html",
-//     reportSuiteAsScenarios: true,
-//     scenarioTimestamp: true,
-//     launchReport: true,
-//     columnLayout: 1,
-// };
+const options = {
+    theme: "bootstrap",
+    jsonDir: "src/reports/",
+    output: "src/reports/cucumber_report.html",
+    reportSuiteAsScenarios: true,
+    scenarioTimestamp: true,
+    launchReport: true,
+    columnLayout: 1,
+};
 
-// reporter.generate(options);
+reporter.generate(options);
