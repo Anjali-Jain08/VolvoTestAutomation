6caed28c48afa8e148506272808ce7de1561c2c9
Anjali-Jain08 <60877575+Anjali-Jain08@users.noreply.github.com>
2024-07-31 16:55:54 +0530

    Update getElementLocators.ts

--- src/locators/getElementLocators.ts
+++ src/locators/getElementLocators.ts
@@ -1,35 +1,35 @@
-// /* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-var-requires */
 
-// // import dataQaIds from '../../src/dataModel/dataQaIds';
-// import homeScreen from "./HomeScreen_Locators";
-// import { waitforTimeout } from "../config/base.config";
+// import dataQaIds from '../../src/dataModel/dataQaIds';
+import homeScreen from "./HomeScreen_Locators";
+import { waitforTimeout } from "../config/base.config";
 
-// const selectorsMap = {
-//     ...homeScreen,
-// };
+const selectorsMap = {
+    ...homeScreen,
+};
 
-// const getElement = (testElement): WebdriverIO.Element => {
-//     const element = $(selectorsMap[testElement]);
-//     element.waitForDisplayed({ timeout: waitforTimeout });
-//     return element;
-// };
+const getElement = (testElement): WebdriverIO.Element => {
+    const element = $(selectorsMap[testElement]);
+    element.waitForDisplayed({ timeout: waitforTimeout });
+    return element;
+};
 
-// const getElements = (testElement): WebdriverIO.ElementArray => {
-//     // console.log("Enter the get element array");
-//     const element = $$(selectorsMap[testElement]);
-//     // console.log("Enter the get element array 2");
-//     // element.waitForDisplayed({ timeout: waitforTimeout });
-//     return element;
-// };
+const getElements = (testElement): WebdriverIO.ElementArray => {
+    // console.log("Enter the get element array");
+    const element = $$(selectorsMap[testElement]);
+    // console.log("Enter the get element array 2");
+    // element.waitForDisplayed({ timeout: waitforTimeout });
+    return element;
+};
 
-// const getLocator = (testElement): WebdriverIO.Element => {
-//     const element = selectorsMap[testElement];
-//     return element;
-// };
+const getLocator = (testElement): WebdriverIO.Element => {
+    const element = selectorsMap[testElement];
+    return element;
+};
 
-// const getElementExists = (testElement): WebdriverIO.Element => {
-//     const element = $(selectorsMap[testElement]);
-//     element.isExisting();
-//     return element;
-// };
-// export { getElement, getLocator, getElementExists, getElements };
+const getElementExists = (testElement): WebdriverIO.Element => {
+    const element = $(selectorsMap[testElement]);
+    element.isExisting();
+    return element;
+};
+export { getElement, getLocator, getElementExists, getElements };
