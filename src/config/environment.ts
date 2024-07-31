03d7cdf98f738809836e2157feca19b47a37c055
Anjali-Jain08 <60877575+Anjali-Jain08@users.noreply.github.com>
2024-07-31 16:54:38 +0530

    Update environment.ts

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,13 +1,13 @@
-// /* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-var-requires */
 
-// const envMap = {
-//   test: {
-//        'urlVolvo': 'https://www.volvocars.com/intl/v/car-safety/a-million-more',
+const envMap = {
+  test: {
+       'urlVolvo': 'https://www.volvocars.com/intl/v/car-safety/a-million-more',
        
-//   }
-// };
+  }
+};
 
-// export const geturl = (urls): WebdriverIO.Element => {
-//   return envMap[process.env.ENV][urls];
-// };
+export const geturl = (urls): WebdriverIO.Element => {
+  return envMap[process.env.ENV][urls];
+};
 
